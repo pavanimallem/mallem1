36433fe4367b9bf28b24f45539bb25c044dd65b4
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-10 15:53:44 +0530

    Create guvi19.py

---
+++ guvi19.py
@@ -0,0 +1,10 @@
+x = int(input(""))
+factorial = 1
+if x < 0 :
+	print("factorial number does not exist negative numbers")
+elif x == 0:
+	print("the factorial number of 0 is 1")
+else:
+	for i in range(1,x+1)
+	factorial =factorial+1
+	print("the factorial number",x,"is"factorial)
